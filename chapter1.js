c2fb172fad01ba3553c2d61cb549e89751a648bf
Kent Tam <ktam39@gmail.com>
2013-08-19 18:04:20 -0700

    added function to determine if a string has all unique characters

--- chapter1.js
+++ chapter1.js
@@ -1,6 +1,31 @@
 var Chapter1 = {
-  StringHasAllUniqueCharacters : function(){
-
+  //Implement an algorithm to determine if a string has all unique characters.
+  StringHasAllUniqueCharacters : function(input){
+    var stringArray = input.split(""),
+        x,
+        cacheObject = {};
+    for(x = 0; x < stringArray.length; x++){
+      if(cacheObject[stringArray[x]] === undefined){
+        cacheObject[stringArray[x]] = "found";
+      } 
+      else{
+        return false;
+      }
+    }
+    return true;
+  },
+  //What if you cannot use additional data structure?
+  UniqueCharactersNoHash: function(input){
+    var stringArray = input.split(""),
+        x, y;  
+    for(x = 0; x < stringArray.length; x++){
+      for(y = x+1; y < stringArray.length; y++){
+        if(stringArray[x] === stringArray[y]){
+          return false; 
+        } 
+      } 
+    }
+    return true;
   }
 };
 
--- spec/chapter1.spec.coffee
+++ spec/chapter1.spec.coffee
@@ -5,3 +5,7 @@ describe "Chapter 1", ->
     it "can tell if a string has all unqiue characters or not", ->
       expect(Chapter1.StringHasAllUniqueCharacters("abcde")).toBe(true)
       expect(Chapter1.StringHasAllUniqueCharacters("accde")).toBe(false)
+    it "can do so without a Hash", ->
+      expect(Chapter1.UniqueCharactersNoHash("abcde")).toBe(true)
+      expect(Chapter1.UniqueCharactersNoHash("accde")).toBe(false)
+
